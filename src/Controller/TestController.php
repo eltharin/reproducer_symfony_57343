cf32c281483c1c2e659843f9b82f8cf9901e1d35
eltharin <eltharin18@hotmail.fr>
2024-06-30 18:14:36 +0200

    first commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsCsrfTokenValid;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    #[IsCsrfTokenValid('notARealCsrfToken')]
+    public function index(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+}
